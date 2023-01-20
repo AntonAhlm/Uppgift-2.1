805aec0e5b09e09fc1e6d2ab39498d61ff71bad5
Anton <>
2023-01-20 09:09:49 +0100

    Add project files.

---
+++ Uppgift 2.1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Vilken stad kommer du ifrån? ");
+            string stad = Console.ReadLine(); 
+            Console.Write("Vilket är ditt drömresemål? ");
+            string resemål = Console.ReadLine();    
+            Console.Write("Hej " + (stad) + "-bo. Hoppas du får resa till " + (resemål));
+
+        }
+    }
+}
